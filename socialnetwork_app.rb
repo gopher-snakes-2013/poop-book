c6d261e54a5a2d40f5e4c7eac53b166681d5676a
Apprentice <apprentice@dbc13.local>
2013-10-08 16:55:17 -0700

    created the users table

---
+++ socialnetwork_app.rb
@@ -0,0 +1,17 @@
+$LOAD_PATH.unshift(File.expand_path('.'))
+
+require 'sinatra'
+require 'sinatra/activerecord'
+require_relative 'models/user'
+
+begin
+require 'dotenv'
+Dotenv.load
+rescue LoadError
+end
+
+set :database, ENV['DATABASE_URL']
+
+get '/' do
+  "Hello, world"
+end
