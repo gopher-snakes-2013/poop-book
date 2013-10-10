097fcb875850dd452b0819a38dc2ff307a945036
Apprentice <apprentice@dbc17.local>
2013-10-10 16:39:35 -0700

    created posts that correspond to users.

--- socialnetwork_app.rb
+++ socialnetwork_app.rb
@@ -13,7 +13,8 @@ ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://local
 enable :sessions
 
 post '/posts' do
-  Post.create(:content => params["user_input"])
+  @logged_in_user = User.find(session["user_id"])
+  @logged_in_user.posts << Post.create(:content => params["user_input"])
   redirect '/user/:username' 
 end
 
@@ -22,8 +23,8 @@ get '/' do
 end
 
 get "/user/:username" do 
-  @username = session["user"]
-  @posts = Post.all.reverse
+  @logged_in_user = User.find(session["user_id"])
+  @posts = Post.where(user_id: @logged_in_user.id).reverse
   erb :user 
 end
 
@@ -33,16 +34,15 @@ end
 
 post '/signup' do
   current_user = User.create!(username: params[:sign_up_user_name], password: params[:sign_up_password])
-  session["user"] = current_user
-
+  session["user_id"] = current_user.id
   redirect "/user/#{current_user.username}"
 end
 
 post '/login' do
   current_user = User.find_by username: params[:login_user_name]
-  session["user"] = current_user
-  if ! current_user.nil?
+  unless current_user.nil?
     if current_user.password == params[:login_password]
+      session["user_id"] = current_user.id
       redirect "/user/#{current_user.username}"
     else
       redirect '/incorrect-login'
