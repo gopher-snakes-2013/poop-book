726a57d73b10c5af722df6f0b40e4c3db3ee6a86
Apprentice <apprentice@dbc16.local>
2013-10-11 15:44:38 -0700

    testing deploy to heroku.

--- Gemfile
+++ Gemfile
@@ -4,12 +4,14 @@ ruby '1.9.3'
 gem 'sinatra'
 gem 'sinatra-activerecord'
 gem 'shotgun'
+# gem 'bcrypt'
 
 group :development do
   gem 'rspec'
   gem 'capybara'
   gem 'shoulda-matchers'
   gem 'dotenv'
+  gem 'sqlite3' # Remove this when deploying to Heroku
 end
 
 group :production do
--- models/user.rb
+++ models/user.rb
@@ -1,9 +1,23 @@
 require './socialnetwork_app'
 
+# require 'bcrypt'
+
 class User < ActiveRecord::Base
+  # include BCrypt
+
   validates_presence_of :username
   validates_presence_of :password
   validates_uniqueness_of :username
 
   has_many :posts
+
+  # def password
+  #   @password ||= Password.new(password_hash)
+  # end
+
+  # def password=(new_password)
+  #   @password = Password.create(new_password)
+  #   self.password_hash = @password
+  # end
+
 end
